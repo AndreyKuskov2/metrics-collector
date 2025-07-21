07437e1b7594f016239b7aa30b207e04e6ffc34d
Andrey Kuskov <andrey.kuskov.05@gmail.com>
2025-07-21 21:03:09 +0300

    iter20

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,21 +18,12 @@ import (
 )
 
 var (
-	buildVersion string
-	buildDate    string
-	buildCommit  string
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
 )
 
 func main() {
-	if buildVersion == "" {
-		buildVersion = "N/A"
-	}
-	if buildDate == "" {
-		buildDate = "N/A"
-	}
-	if buildCommit == "" {
-		buildCommit = "N/A"
-	}
 	fmt.Printf("Build version: %s\n", buildVersion)
 	fmt.Printf("Build date: %s\n", buildDate)
 	fmt.Printf("Build commit: %s\n", buildCommit)
